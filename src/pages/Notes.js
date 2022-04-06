80d17ab05f38a5e79fc38c9731a4c15026c1693d
Rahiful Islam <rahiful@uxd.co.jp>
2022-04-06 12:06:14 +0600

    first commit

---
+++ src/pages/Notes.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Typography } from '@mui/material';
+
+const Notes = () => {
+  return (
+    <div>
+      <Typography
+        variant="h6"
+        color="textSecondary"
+        component="h2"
+        gutterBottom
+      >
+        Create New Notes
+      </Typography>
+
+    </div>
+  )
+}
+
+export default Notes
